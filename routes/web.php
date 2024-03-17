b16b7e862c81eecd2abb6269c3f9c36a0ca8b2fe
zidniahmeed <zidniahmeed@gmail.com>
2024-03-17 19:38:43 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('notify',[HomeController::class, 'notify']);
+// Route::get('notify',[HomeController::class, 'notify']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
